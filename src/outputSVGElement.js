b18db3a06397058126858830566752e3013346b7
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-20 15:40:50 -0600

    SVG graphics containers and text elements in d3

--- src/outputSVGElement.js
+++ src/outputSVGElement.js
@@ -6,17 +6,22 @@ let scores = [
   { name: 'Emily', score: 88}
 ];
 
-d3.select('.chart')
+let bar = d3.select('.chart')
   .append('svg')
     .attr('width', 225)
     .attr('height', 300)
-  .selectAll('rect')
+  .selectAll('g')
   .data(scores)
   .enter()
-  .append('rect')
+  .append('g')
   // d is the first element represents the data and i is the index
-  .attr('y', (d, i) => i * 33)
+  .attr('transform', (d, i) => 'translate(0,' + i * 33+')');
+
+bar.append('rect')
   .style('width', d => d.score + 'px')
-  .text((d) => d.name)
   .attr('class', 'bar');
+
+bar.append('text')
+  .attr('y', 20)
+  .text((d) => d.name);
 
