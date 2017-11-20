5f2978e5001c65526ceed0f00cb2e55fca40e557
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-19 20:32:46 -0600

    create labels from non-numeric data with ordinal scales

---
+++ src/scaleOrdinal.js
@@ -0,0 +1,7 @@
+const ordinalScale = d3.scaleOrdinal()
+  .domain(["poor", "good", "great"])
+  .range(["red", "white", "green"]);
+
+console.log(ordinalScale("good"));
+console.log(ordinalScale("great"));
+console.log(ordinalScale("poor"));
