5431fb32a25c26f4d1f1b70291ac74c15dceedce
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-20 19:42:59 -0600

    build a scatter plot with d3

---
+++ src/scatterPlot.js
@@ -0,0 +1,89 @@
+let margin = {top: 20, right: 40, bottom: 50, left: 40};
+let width = 425 - margin.left - margin.right;
+let height = 625 - margin.top - margin.bottom;
+
+let fullWidth = width + margin.left + margin.right;
+let fullHeight = height + margin.top + margin.bottom;
+
+let svg = d3.select('.chart')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .call(responsivefy)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+d3.json('../data/countryData.json', function (err, data) {
+  let yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.expectancy))
+    .range([height, 0])
+    .nice();
+  let yAxis = d3.axisLeft(yScale);
+  svg.call(yAxis);
+
+  let xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.cost))
+    .range([0, width])
+    .nice();
+  let xAxis = d3.axisBottom(xScale).ticks(5);
+  svg
+    .append('g')
+      .attr('transform', `translate(0, ${height})`)
+    .call(xAxis);
+
+  let rScale = d3.scaleSqrt()
+    .domain([0, d3.max(data, d => d.population)])
+    .range([0, 40]);
+
+  let circles = svg
+    .selectAll('.ball')
+    .data(data)
+    .enter()
+    .append('g')
+      .attr('class', 'ball')
+      .attr('transform', d => {
+        return `translate(${xScale(d.cost)}, ${yScale(d.expectancy)})`
+      });
+
+  circles
+    .append('circle')
+    .attr('cx', 0)
+    .attr('cy', 0)
+    .attr('r', d => rScale(d.population))
+    .style('fill', 'steelblue')
+    .style('fill-opacity', 0.5);
+
+  circles
+    .append('text')
+    .style('text-anchor', 'middle')
+    .style('fill', 'black')
+    .attr('y', 4)
+    .text(d => d.code)
+});
+
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  let container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style("width")),
+    height = parseInt(svg.style("height")),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    let targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
